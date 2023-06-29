6e34f19c8460c7dafe00d74fe130fcecf2c9afdf
Ross M. Lodge <ross@projectronin.com>
2023-06-29 10:01:56 -0700

    Update to new version of tooling

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "contract-messaging-document-data"
+
+pluginManagement {
+    plugins {
+        id("com.projectronin.event.contract") version "2.0.0-feature-code-generation-SNAPSHOT"
+    }
+
+    repositories {
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-public/")
+        }
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-public/")
+        }
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
